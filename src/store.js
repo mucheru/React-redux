8e28ddf47810231b3a7dd038698b21a5947ca32d
steve <stephen@stephenmucheru79@gmail.com>
2023-08-08 11:58:40 +0300

    store file + feature file that have components

---
+++ src/store.js
@@ -0,0 +1,10 @@
+import { configureStore } from "@reduxjs/toolkit";
+
+import todoReducer from './features/todo/todoSlice';
+
+export default configureStore({
+    reducer:{
+        todos: todoReducer,
+
+    },
+})
